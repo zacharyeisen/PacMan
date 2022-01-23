c620961235fe2e0da97d030957ff667069c7a6ed
zacharyeisen <93158135+zacharyeisen@users.noreply.github.com>
2022-01-22 16:36:08 -0800

    Update Pacmen.js

--- Pacmen.js
+++ Pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-  ["./PacMan1.png", "./PacMan2.png"],
-  ["./PacMan3.png", "./PacMan4.png"],
+  ["PacMan1.png", "PacMan2.png"],
+  ["PacMan3.png", "PacMan4.png"],
 ];
 var direction = 0;
 const pacMen = [];
